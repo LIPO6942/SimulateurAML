56812190f5e756c43dced6b590ef0ce1b656c7b8
moslem.gouia <moslem.gouia@gmail.com>
2026-02-18 12:21:42 +0000

    fix: Correct Firebase config syntax

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from \"firebase/app\";
-import { getFirestore } from \"firebase/firestore\";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +8,13 @@ import { getFirestore } from \"firebase/firestore\";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: \"AIzaSyCH7Am76Pit5KDosIK8qSDJRm9r3wIF2Yw",
-  authDomain: \"simulateur-aml.firebaseapp.com",
-  projectId: \"simulateur-aml",
-  storageBucket: \"simulateur-aml.firebasestorage.app",
-  messagingSenderId: \"355107034345",
-  appId: \"1:355107034345:web:62ecbe3731c4e332894c78",
-  measurementId: \"G-LBRQCQ00JX"
+  apiKey: "AIzaSyCH7Am76Pit5KDosIK8qSDJRm9r3wIF2Yw",
+  authDomain: "simulateur-aml.firebaseapp.com",
+  projectId: "simulateur-aml",
+  storageBucket: "simulateur-aml.firebasestorage.app",
+  messagingSenderId: "355107034345",
+  appId: "1:355107034345:web:62ecbe3731c4e332894c78",
+  measurementId: "G-LBRQCQ00JX"
 };
 
 // Initialize Firebase
